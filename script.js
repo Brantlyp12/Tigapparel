6145fb94d43851a35a6f82d954826b25247c3584
Brantlyp12 <brantlyphillips12@gmail.com>
2025-02-28 15:22:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
+// JavaScript for adding items to the cart
 function addToCart(productName) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
     cart.push(productName);
     localStorage.setItem('cart', JSON.stringify(cart));
     alert(productName + " has been added to your cart!");
 }
+
+// Attach event listeners to buttons
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.querySelectorAll(".add-to-cart");
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            let productName = this.dataset.product;
+            addToCart(productName);
+        });
+    });
+});
